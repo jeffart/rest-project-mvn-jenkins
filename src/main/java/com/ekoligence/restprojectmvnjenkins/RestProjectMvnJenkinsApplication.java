74871b7c53726991ab8574fc12148f326a128767
jeffart <dekogha@gmail.com>
2021-09-19 23:37:55 +0200

    First commit

---
+++ src/main/java/com/ekoligence/restprojectmvnjenkins/RestProjectMvnJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.ekoligence.restprojectmvnjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestProjectMvnJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestProjectMvnJenkinsApplication.class, args);
+	}
+
+}
